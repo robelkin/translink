be8e9a74d0cd0a978c74f3ce800507c1be5c567e
Rob Elkin <rob@infinity21.net>
2010-01-24 15:29:12 +0000

    Added the AllRoutes method to get all the routes possible out

--- metro/api/Journey.php
+++ metro/api/Journey.php
@@ -4,7 +4,7 @@ class Journey extends Base
 {
 	public function __construct( $data = "", $params = array() )
 	{
-		parent::__construct();
+		parent::__construct( $data, $params );
 		$this->data = $data;
 		$this->params = $params;
 	}
@@ -14,8 +14,11 @@ class Journey extends Base
 		{
 			switch( $this->data )
 			{
-				case "Routes":
-					
+				case "AllRoutes":
+					$sql = "SELECT RouteNumber, RunningBoard, UniqueJourneyIdentifier FROM tblJourney";
+					$results = DataHelper::LoadTableFromSql( $sql );
+					print json_encode( $results );
+					exit;
 				break;
 				default:
 					throw new Exception( "Invalid Object Requested", 03 );
