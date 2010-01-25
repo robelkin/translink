d7c51c307e88d24213b6e3fe589f14dcc9313a99
Alex <alex@alexmcroberts.co.uk>
2010-01-25 23:47:06 +0000

    modified:   Stop.php

--- metro/api/Stop.php
+++ metro/api/Stop.php
@@ -41,7 +41,7 @@ class Stop extends Base
 						throw new Exception( "Invalid Params pass", 02 );
 					}
 					$sql = sprintf( "SELECT StopID, StopName, 3963.191 * ACOS( (
-SIN( PI( ) * '%1$s' /180 ) * SIN( PI( ) * StopLat /180 ) ) + ( COS( PI( ) * '%1$s' /180 ) * COS( PI( ) * StopLat /180 ) * COS( PI( ) * StopLong /180 - PI( ) * - '%2$s' /180 ) ) ) AS distance FROM tblStop ORDER BY distance LIMIT 0 , 30", $this->params['lat'], $this->params['long']);
+SIN( PI( ) * '%1$d' /180 ) * SIN( PI( ) * StopLat /180 ) ) + ( COS( PI( ) * '%1$d' /180 ) * COS( PI( ) * StopLat /180 ) * COS( PI( ) * StopLong /180 - PI( ) * - '%2$d' /180 ) ) ) AS distance FROM tblStop WHERE distance < '%3$d' ORDER BY distance LIMIT 0 , 30", $this->params['lat'], $this->params['long'], $this->params['distance'] );
 					$results = DataHelper::LoadTableFromSql( $sql );
 					print json_encode( $results);
 					exit;
