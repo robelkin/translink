6e1f247c21dd3d62da47819169be18fdbebd782a
Alex <alex@alex-laptop.(none)>
2010-01-25 15:44:39 +0000

    Included geo-location method

--- metro/api/Stop.php
+++ metro/api/Stop.php
@@ -45,6 +45,16 @@ class Stop extends Base
 					print json_encode( $results );
 					exit;
 				break;
+				case "NearestStop":
+					if( !$this->params['lat'] || !$this->params['long'] )
+					{
+						throw new Exception( "Invalid Params pass", 02 );
+					}
+					$sql = sprintf( "SELECT StopID, StopName, 3963.191 * ACOS( (
+SIN( PI( ) * '%1$s' /180 ) * SIN( PI( ) * StopLat /180 ) ) + ( COS( PI( ) * '%1$s' /180 ) * COS( PI( ) * StopLat /180 ) * COS( PI( ) * StopLong /180 - PI( ) * - '%2$s' /180 ) ) ) AS distance FROM tblStop ORDER BY distance LIMIT 0 , 30", $this->params['lat'], $this->params['long']);
+					$results = DataHelper::LoadTableFromSql( $sql );
+					print json_encode( $results);
+					exit;
 				default:
 					throw new Exception( "Invalid Object Requested", 03 );
 				break;
